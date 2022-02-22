55907c00a885e7d350d53dca9c0f7bd0f543f11f
Danielle Bencze <daniellebencze@live.com>
2022-02-21 22:03:17 -0800

    commit tree 8701570a446a833a284393ef301a5d1631b946f5

--- spec/environment_spec.rb
+++ spec/environment_spec.rb
@@ -1,3 +1,6 @@
+require_relative '../lib/garden'
+require_relative '../lib/plant'
+
 describe "environment.rb" do
   it "loads the code for the Garden and Plant classes" do
     expect { Garden }.not_to raise_error
